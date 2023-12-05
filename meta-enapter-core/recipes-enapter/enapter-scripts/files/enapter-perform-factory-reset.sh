0571b58b8e8ed1423b86f147685dd3e8f167c9d1
Dmitry Myaskovskiy <dmitry@enapter.com>
2023-12-05 21:26:05 +0100

    Factory reset and image update fix

--- meta-enapter-core/recipes-enapter/enapter-scripts/files/enapter-perform-factory-reset.sh
+++ meta-enapter-core/recipes-enapter/enapter-scripts/files/enapter-perform-factory-reset.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: Apache-2.0
 
 if [ -L /dev/disk/by-label/enp-data-disk ] ; then
-  /sbin/e2label /dev/disk/by-label/enp-data-disk "enp-data-disk-reset" && /sbin/reboot -f
+  /sbin/e2label /dev/disk/by-label/enp-data-disk "enp-data-reset" && sleep 1 && /sbin/reboot -f
 fi
 
 echo "User data disk is not set. Please set it by 'set-data-disk' command"
