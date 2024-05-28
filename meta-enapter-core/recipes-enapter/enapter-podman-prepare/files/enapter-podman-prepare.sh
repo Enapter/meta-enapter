77d83f5d0ced13f77d10f159d3d1e1731de860e7
Dmitry Myaskovskiy <dmitry@enapter.com>
2024-05-28 14:57:00 +0200

    Fix images loading for case when multiple layers exists

--- meta-enapter-core/recipes-enapter/enapter-podman-prepare/files/enapter-podman-prepare.sh
+++ meta-enapter-core/recipes-enapter/enapter-podman-prepare/files/enapter-podman-prepare.sh
@@ -8,9 +8,7 @@ podman_storage_config="/etc/containers/storage.conf"
 layers_dir="/layers"
 
 if [[ -f "$podman_storage_config" && -d "$layers_dir" ]]; then
-  layers=$(find "$layers_dir" -maxdepth 1 -mindepth 1 -type d)
-
-  for l in "$layers"; do
+  find "$layers_dir" -maxdepth 1 -mindepth 1 -type d -print0 | while IFS= read -r -d '' l; do
     if [[ -f "$l/images/overlay-images/images.lock" ]]; then
       sed -i "/# \\\$ADDITIONALIMAGESTORES\\\$/a \\ \\ ,'$l/images'" "$podman_storage_config"
     fi
