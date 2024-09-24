1e540017cf9cebf62cdd020fedf1bafdd2720151
Dmitry Myaskovskiy <dmitry@enapter.com>
2024-09-24 21:59:30 +0200

    Cleanup

--- meta-enapter-core/recipes-enapter/enapter-scripts/files/enapter-set-userspace-disk.sh
+++ meta-enapter-core/recipes-enapter/enapter-scripts/files/enapter-set-userspace-disk.sh
@@ -33,32 +33,34 @@ sfdisk --delete "$disk" || true
 
 bootloader_part="start=2m size=16m name=$disk_bootloader_label type=U"
 config_part="start=64m size=16m name=$disk_config_label type=L"
-kernel_1_part="size=128m name=$disk_kernel_1_label type=L"
-kernel_2_part="size=128m name=$disk_kernel_2_label type=L"
-root_1_part="size=1536m name=$disk_root_1_label type=L"
-root_2_part="size=1536m name=$disk_root_2_label type=L"
-app_1_part="size=4096m name=$disk_app_1_label type=L"
-app_2_part="size=4096m name=$disk_app_2_label type=L"
+kernel_a_part="size=128m name=$disk_kernel_a_label type=L"
+root_a_part="size=1536m name=$disk_root_a_label type=L"
+app_a_part="size=4096m name=$disk_app_a_label type=L"
+kernel_b_part="size=128m name=$disk_kernel_b_label type=L"
+root_b_part="size=1536m name=$disk_root_b_label type=L"
+app_b_part="size=4096m name=$disk_app_b_label type=L"
 backup_part="size=512m name=$disk_backup_label type=L"
 images_part="size=16384m name=$disk_images_label type=L"
 user_part="name=$disk_data_label type=L"
 
 # shellcheck disable=SC2059
-printf "$bootloader_part\n $config_part\n $kernel_1_part\n $kernel_2_part\n $root_1_part\n $root_2_part\n $app_1_part\n $app_2_part\n $backup_part\n $images_part\n $user_part" | \
+printf "$bootloader_part\n $config_part\n $kernel_a_part\n $root_a_part \n$app_a_part\n $kernel_b_part\n $root_b_part\n $app_b_part\n $backup_part\n $images_part\n $user_part" | \
   sfdisk --label gpt --force "$disk" -w always -W always
 
 udevadm trigger --action=add
 udevadm settle || sleep 3
 
+# TODO: check if disk size at least ??? GB
+
 # do not need to format bootloader partition
 # create_vfat_fs "$disk_bootloader_label" || fatal "Bootloader FS creation failed"
 create_fs "$disk_config_label" || fatal "Configuration FS creation failed"
-create_fs "$disk_kernel_1_label" || fatal "Kernel 1 FS creation failed"
-create_fs "$disk_kernel_2_label" || fatal "Kernel 2 FS creation failed"
-create_fs "$disk_root_1_label" || fatal "Root 1 FS creation failed"
-create_fs "$disk_root_2_label" || fatal "Root 2 FS creation failed"
-create_fs "$disk_app_1_label" || fatal "Application 1 FS creation failed"
-create_fs "$disk_app_2_label" || fatal "Application 2 FS creation failed"
+create_fs "$disk_kernel_a_label" || fatal "Kernel 1 FS creation failed"
+create_fs "$disk_root_a_label" || fatal "Root 1 FS creation failed"
+create_fs "$disk_app_a_label" || fatal "Application 1 FS creation failed"
+create_fs "$disk_kernel_b_label" || fatal "Kernel 2 FS creation failed"
+create_fs "$disk_root_b_label" || fatal "Root 2 FS creation failed"
+create_fs "$disk_app_b_label" || fatal "Application 2 FS creation failed"
 create_fs "$disk_backup_label" || fatal "Backup FS creation failed"
 create_fs "$disk_images_label" || fatal "Images FS creation failed"
 create_fs "$disk_data_label" || fatal "User FS creation failed"
